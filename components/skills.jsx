ff919c09688f44a3c94b792bbbe77377126d1887
aditya <adityavadityav@gmail.com>
2021-04-12 23:43:47 +0530

    UI changes

--- components/skills.jsx
+++ components/skills.jsx
@@ -45,7 +45,7 @@ const Skills = () => {
                     <div className="col-lg-6">
 
                         <div className="progress">
-                            <span className="skill">PHP <i className="val">80%</i></span>
+                            <span className="skill">AWS <i className="val">80%</i></span>
                             <div className="progress-bar-wrap">
                                 <div className="progress-bar" role="progressbar" aria-valuenow="80"
                                      aria-valuemin="0" aria-valuemax="100"></div>
@@ -53,7 +53,7 @@ const Skills = () => {
                         </div>
 
                         <div className="progress">
-                            <span className="skill">WordPress/CMS <i className="val">90%</i></span>
+                            <span className="skill">Next <i className="val">90%</i></span>
                             <div className="progress-bar-wrap">
                                 <div className="progress-bar" role="progressbar" aria-valuenow="90"
                                      aria-valuemin="0" aria-valuemax="100"></div>
@@ -61,7 +61,7 @@ const Skills = () => {
                         </div>
 
                         <div className="progress">
-                            <span className="skill">Photoshop <i className="val">55%</i></span>
+                            <span className="skill">Angular <i className="val">55%</i></span>
                             <div className="progress-bar-wrap">
                                 <div className="progress-bar" role="progressbar" aria-valuenow="55"
                                      aria-valuemin="0" aria-valuemax="100"></div>
